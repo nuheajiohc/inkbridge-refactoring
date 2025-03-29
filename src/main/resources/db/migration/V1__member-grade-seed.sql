b8f2fbaedf7320b5eb0add0d62406f482e574e8b
jaehun <jaehun341012@gmail.com>
2025-03-29 22:35:30 +0900

    feat: 회원등급 애플리케이션 시작 시 초기화

---
+++ src/main/resources/db/migration/V1__member-grade-seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO grade(name, earning_rate, min_purchase_amount, created_at)
+    VALUES ("STANDARD", 1,0, now()),
+           ("ROYAL",2,100000,now()),
+           ("GOLD",3,200000,now()),
+           ("PLATINUM", 4,300000,now())

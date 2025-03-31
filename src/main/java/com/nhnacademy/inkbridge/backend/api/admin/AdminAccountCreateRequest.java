e8650d0fe02948c0b98b1e981568556214d8e52b
최재훈 <jaehun341012@gmail.com>
2025-03-31 19:16:19 +0900

    feat: 관리자계정생성 유효성검사

--- src/main/java/com/nhnacademy/inkbridge/backend/api/admin/AdminAccountCreateRequest.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/api/admin/AdminAccountCreateRequest.java
@@ -1,6 +1,9 @@
 package com.nhnacademy.inkbridge.backend.api.admin;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.nhnacademy.inkbridge.backend.domain.admin.Admin;
 
@@ -9,8 +12,14 @@ import lombok.Getter;
 @Getter
 public class AdminAccountCreateRequest {
 
+	@NotBlank
+	@Length(min = 2, max = 20)
 	private final String name;
+	@NotBlank
+	@Pattern(regexp="^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message="이메일 주소 양식을 확인해주세요")
 	private final String email;
+	@NotBlank
+	@Length(min = 6, max = 20)
 	private final String password;
 
 	public AdminAccountCreateRequest(String name, String email, String password) {
